97047d2cfd8d3b3a28dc0718e42c69a5c7fa273d
Ange804 <kanakimanamarieange8@gmail.com>
2026-07-22 17:21:45 +0200

    servicecarte

--- src/app/modules/services-publics/services-publics.module.ts
+++ src/app/modules/services-publics/services-publics.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ServicesPublicsRoutingModule } from './services-publics-routing.module';
-
+import { ServicesPublicsListComponent } from './pages/services-publics-list/services-publics-list.component';
+import { ServicesPublicsDetailComponent } from './pages/services-publics-detail/services-publics-detail.component';
+import { ServicesPublicsEditComponent } from './pages/services-publics-edit/services-publics-edit.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ServicesPublicsListComponent,
+    ServicesPublicsDetailComponent,
+    ServicesPublicsEditComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
+    HttpClientModule,
     ServicesPublicsRoutingModule
   ]
 })
